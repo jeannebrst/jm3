1b6a859bbb8ba7bba6f10449fe895d7d0a2101a4
BARASTIER JEANNE <jbarastie820@u-001-5.univ-tln.fr>
2025-02-28 11:27:11 +0100

    OMG LES ANNEAUUUUUX

--- src/main/java/fr/utln/jmonkey/tutorials/beginner/projetTP/Planet.java
+++ src/main/java/fr/utln/jmonkey/tutorials/beginner/projetTP/Planet.java
@@ -2,17 +2,20 @@ package fr.utln.jmonkey.tutorials.beginner.projetTP;
 
 
 import com.jme3.material.Material;
+import com.jme3.material.RenderState;
+import com.jme3.renderer.queue.RenderQueue;
 import com.jme3.math.FastMath;
+import com.jme3.math.Vector2f;
+import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Sphere;
+import com.jme3.texture.Texture;
 import com.jme3.scene.Mesh;
 import com.jme3.scene.VertexBuffer;
 import com.jme3.util.BufferUtils;
 import com.jme3.scene.Node;
 import com.jme3.asset.AssetManager;
 import java.util.*;
-
-import java.nio.FloatBuffer;
 
 public class Planet {
 	private String nom;
@@ -94,34 +97,56 @@ public class Planet {
 
 	public void addRings(AssetManager assetManager, String nomTexture) {
 			Mesh ringMesh = new Mesh();
-			int numPoints = 800000;
-			FloatBuffer vertexBuffer = BufferUtils.createFloatBuffer(numPoints * 3);
+			int segments = 128;
+			Vector3f[] sommets = new Vector3f[segments*2];
+			Vector2f[] coords = new Vector2f[segments*2];
+
+			int[] indices = new int[segments * 6];
 	
 			// float innerRingRadius = taillePlanete + 10f;
 			// float outerRingRadius = taillePlanete + 60f;
 
-			float innerRingRadius = 66;
-			float outerRingRadius = 120;
+			float rayonInterne = 66;
+			float rayonExterne = 120;
 	
-			for (int i = 0; i < numPoints; i++) {
-				float angle = FastMath.nextRandomFloat() * FastMath.TWO_PI;
-				float radius = innerRingRadius + FastMath.nextRandomFloat() * (outerRingRadius - innerRingRadius);
-				float x = FastMath.cos(angle) * radius;
-				float y = 0;
-				float z = FastMath.sin(angle) * radius;
-				vertexBuffer.put(x).put(y).put(z);
+			for (int i = 0; i < segments; i++) {
+				float angle = (float) (i*FastMath.PI*2/segments);
+				float cos = (float) FastMath.cos(angle);
+				float sin = (float) FastMath.sin(angle);
+
+				sommets[i*2] = new Vector3f(rayonInterne*cos,0,rayonInterne*sin);
+				sommets[i*2+1] = new Vector3f(rayonExterne*cos,0,rayonExterne*sin);
+
+				coords[i*2] = new Vector2f(0,i/(float) segments);
+				coords[i*2+1] = new Vector2f(1,i/(float) segments);
+
+				int next = (i + 1) % segments;
+				
+				indices[i * 6] = i * 2;
+				indices[i * 6 + 1] = next * 2;
+				indices[i * 6 + 2] = i * 2 + 1;
+
+				indices[i * 6 + 3] = i * 2 + 1;
+				indices[i * 6 + 4] = next * 2;
+				indices[i * 6 + 5] = next * 2 + 1;
 			}
 	
-			ringMesh.setBuffer(VertexBuffer.Type.Position, 3, vertexBuffer);
-			ringMesh.setMode(Mesh.Mode.Points);
+			ringMesh.setBuffer(VertexBuffer.Type.Position, 3, BufferUtils.createFloatBuffer(sommets));
+			ringMesh.setBuffer(VertexBuffer.Type.TexCoord, 2, BufferUtils.createFloatBuffer(coords));
+			ringMesh.setBuffer(VertexBuffer.Type.Index, 3, BufferUtils.createIntBuffer(indices));
 			ringMesh.updateBound();
-			ringMesh.updateCounts();
 	
 			Geometry ringGeo = new Geometry("Saturn Rings", ringMesh);
 			Material ringMat = new Material(assetManager,
-			"Common/MatDefs/Light/Lighting.j3md");
-			ringMat.setTexture("DiffuseMap", assetManager.loadTexture("Textures/Terrain/"+nomTexture+".png"));
+			"Common/MatDefs/Misc/Unshaded.j3md");
+			Texture ringTex = assetManager.loadTexture("Textures/Terrain/"+nomTexture+".png");
+			ringTex.setWrap(Texture.WrapMode.EdgeClamp);
+
+			ringMat.setTexture("ColorMap", ringTex);
+			ringMat.getAdditionalRenderState().setBlendMode(com.jme3.material.RenderState.BlendMode.Alpha);
+			ringMat.getAdditionalRenderState().setFaceCullMode(RenderState.FaceCullMode.Off);
 			ringGeo.setMaterial(ringMat);
+			ringGeo.setQueueBucket(RenderQueue.Bucket.Transparent);
 	
 			anneaux = new Node("anneaux");
 			anneaux.attachChild(ringGeo);
